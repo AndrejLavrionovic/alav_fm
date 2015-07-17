6f5ba8be8a7e13bac46f9251d1bd8caa149c91e8
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2015-07-17 13:10:19 +0100

    Creating new class Person

---
+++ src/units/Person.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package units;
+
+/**
+ *
+ * @author Andrej Lavrinovic
+ * Made by NetBeands v8.0.2
+ * Java v1.8_0_20
+ * Date: 17/07/2015
+ * Time: 12:51
+ * Last changed: 
+ */
+public class Person {
+    
+}
